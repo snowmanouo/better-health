bceeb3cf0befac12df34106bdcc52571bc23a160
snowmanouo <106911253+snowmanouo@users.noreply.github.com>
2022-08-13 01:25:59 +0800

    Add firebase to the APP

--- lib/main.dart
+++ lib/main.dart
@@ -5,23 +5,32 @@ import 'Page/Page1.dart';
 import 'package:flutter/services.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() => runApp(ProjectApp());
 
 class ProjectApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: new ThemeData(
-        primaryColor: appGreenColor,
-      ),
-      home: HealthPage(),
+    return FutureBuilder(
+        future: Firebase.initializeApp(
+            options: DefaultFirebaseOptions.currentPlatform
+        ),
+        builder: (context, snapshot) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: new ThemeData
+              (
+              primaryColor: appGreenColor,
+            ),
+            home: HealthPage(),
+          );
+        },
     );
   }
-
 }
+
 
 
 

8d098d2fd9a23e4a5a51083aa0e2ccc845126e1e
snowmanouo <106911253+snowmanouo@users.noreply.github.com>
2022-09-03 23:35:45 +0800

    finish the search &filter ListView for ExcerciseListPage

--- lib/Page/ExcerciseListPage.dart
+++ lib/Page/ExcerciseListPage.dart
@@ -1,7 +1,29 @@
 import 'package:flutter/material.dart';
 import '../helpers/Constants.dart';
 
-class ExcerciseList extends StatelessWidget {
+class ExcerciseList extends StatefulWidget{
+
+  @override
+  _ExcerciseListState createState() => _ExcerciseListState();
+
+
+}
+
+
+class _ExcerciseListState extends State<ExcerciseList> {
+  final controller = TextEditingController();
+  List<Book> books = allBooks;
+  void searchBook(String query){
+    final suggestions = allBooks.where((book){
+      final bookTitle = book.title.toLowerCase();
+      final input = query.toLowerCase();
+
+      return bookTitle.contains(input);
+    }).toList();
+
+    setState(() => books = suggestions);
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,66 +43,67 @@ class ExcerciseList extends StatelessWidget {
           Column(
             children: [
               Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Container(
-                    width: 247,
-                    alignment: Alignment.centerLeft,
-                    child: Text('運動清單',
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold)),
-                  ),
+                  Text('運動清單',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                  SizedBox(width: 173),
                   Row(
                     children: [
-                      Container(
-                        child: Text('自定義',
-                            style: TextStyle(fontSize: 16, color: Colors.grey)),
-                      ),
-                      SizedBox(
-                        width: 9,
-                      ),
-                      Container(
-                        alignment: Alignment.centerLeft,
-                        child: IconButton(
-                          padding: EdgeInsets.zero,
-                          constraints: BoxConstraints(),
-                          icon: Image.asset('assets/images/addButton.png'),
-                          onPressed: () {},
-                        ),
+                      Text('自定義',
+                          style: TextStyle(fontSize: 16, color: Colors.grey)),
+                      SizedBox(width: 9),
+                      IconButton(
+                        padding: EdgeInsets.zero,
+                        constraints: BoxConstraints(),
+                        icon: Image.asset('assets/images/addButton.png'),
+                        onPressed: () {},
                       ),
                     ],
                   ),
                 ],
               ),
-              Container(
-                child:
-                Card(
-                  color: Color.fromRGBO(238, 238, 238, 1),
-                  child: ListTile(
-                    leading: Icon(Icons.search),
-                    title:  TextField(
-                      decoration:  InputDecoration(
-                        hintText: '搜尋',
-                        hintStyle: TextStyle(
-                          color: Colors.grey,
-                          fontSize: 16,
-                        ),
-                        border: InputBorder.none,
-                      ),
+              SizedBox(height: 24),
+              Column(
+                children: [
+                Container(
+                  height: 40,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: Color.fromRGBO(238, 238, 238, 1.0),
+                    boxShadow: [
+                      BoxShadow(color: Colors.transparent, spreadRadius: 3),
+                    ],
+                  ),
+                  child: TextField(
+                    controller: controller,
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.search, color: Colors.black),
+                      hintText: '搜尋',
+                      hintStyle: TextStyle(fontSize: 16),
+                      border: InputBorder.none,
                     ),
+                    onChanged: searchBook,
                   ),
                 ),
-              ),
-              ListView.builder(
+                ListView.builder(
                   shrinkWrap: true,
-                  itemCount: 8,
-                  itemBuilder: (context, index){
-                    return Row(
-                      children: [
-                        Text('測試'),
-                      ],
+                  itemCount: books.length,
+                  itemBuilder: (context, index) {
+                    final book = books[index];
+                    return ListTile(
+                      leading: Image.network(
+                        book.urlImage,
+                        fit: BoxFit.cover,
+                        width: 50,
+                        height: 50,
+                      ),
+                      title: Text(book.title),
                     );
-                  }),
+                  },
+                ),
+
+              ],),
             ],
           ),
         ],
@@ -88,3 +111,20 @@ class ExcerciseList extends StatelessWidget {
     );
   }
 }
+
+
+class Book {
+  final String title;
+  final String urlImage;
+
+  const Book({
+    required this.title,
+    required this.urlImage,
+  });
+}
+
+const allBooks = [
+  Book(title: 'A', urlImage: "https://static.vecteezy.com/packs/media/vectors/term-bg-1-666de2d9.jpg"),
+  Book(title: 'B', urlImage: "https://static.vecteezy.com/packs/media/vectors/term-bg-1-666de2d9.jpg"),
+  Book(title: 'C', urlImage: "https://static.vecteezy.com/packs/media/vectors/term-bg-1-666de2d9.jpg"),
+];

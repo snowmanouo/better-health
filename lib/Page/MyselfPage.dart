b9d583e559499fbddbfed3854549283cafe42d53
unknown <chenyh8899@gmail.com>
2022-09-12 22:19:03 +0800

    add calendar

--- lib/Page/MyselfPage.dart
+++ lib/Page/MyselfPage.dart
@@ -10,8 +10,13 @@ import 'Calorie.dart';
 import 'Time.dart';
 import 'Kg.dart';
 import 'Fat.dart';
+import 'package:r_calendar/r_calendar.dart';
+import 'Calendar.dart';
 
 class MyselfPage extends StatelessWidget {
+  late DateTime _myDateTime;
+  String time ='?';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -120,34 +125,45 @@ class MyselfPage extends StatelessWidget {
                           primary: Colors.black,
                         ),
                         onPressed: () => showDialog<String>(
-                          context: context,
-                          builder: (BuildContext context) => AlertDialog(
-                            title: Text('每日體重'),
-                            content: const TextField(
-                              obscureText: true,
-                              decoration: InputDecoration(
-                                border: OutlineInputBorder(),
-                                labelText: '請輸入今日體重',
+                              context: context,
+                              builder: (BuildContext context) => AlertDialog(
+                                title: Text('每日體重'),
+
+                                content: const TextField(
+                                  obscureText: true,
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    labelText: '請輸入今日體重',
+                                  ),
+                                ),
+                                actions: <Widget>[
+                                  ElevatedButton(
+                                      onPressed: () async {
+                                        _myDateTime = (await showDatePicker(
+                                          context: context,
+                                          initialDate: DateTime.now(),
+                                          firstDate: DateTime(2010),
+                                          lastDate: DateTime(2025),
+                                        ))!;
+
+                                      },
+                                      child: Text('請選擇日期')),
+                                  TextButton(
+                                    onPressed: () =>
+                                        Navigator.pop(context, 'OK'),
+                                    child: Text('OK'),
+                                  ),
+                                  TextButton(
+                                    onPressed: () =>
+                                        Navigator.pop(context, 'Cancel'),
+                                    child: Text('Cancel'),
+                                  ),
+                                ],
                               ),
                             ),
-                            actions: <Widget>[
-                              TextButton(
-                                onPressed: () => Navigator.pop(context, 'OK'),
-                                child: Text('OK'),
-                              ),
-                              TextButton(
-                                onPressed: () => Navigator.pop(context, 'Cancel'),
-                                child: Text('Cancel'),
-                              ),
-                            ],
-                          ),
-                        ),
-                        child:Row(
-                          children: [
-                            Icon(Icons.settings_applications)
-                          ],
-                        )
-                    ),
+                        child: Row(
+                          children: [Icon(Icons.settings_applications)],
+                        )),
                   ),
                   SizedBox(
                     height: 0,
